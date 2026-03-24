eeb6279bd3d067f9b257d7f2a1309739da7e0588
h678153 <678153@stud.hvl.no>
2026-03-24 13:08:27 +0100

    Initial bot setup with Discord.Net connection

---
+++ Quizbot for Discord/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+
+
+IConfigurationRoot config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+string? token = config["Discord:Token"];
+
+var socketConfig = new DiscordSocketConfig
+{
+    GatewayIntents = GatewayIntents.Guilds | GatewayIntents.GuildMessages
+};
+
+var client = new DiscordSocketClient(socketConfig);
+
+client.Log += LogHandler;
+
+Task LogHandler(LogMessage message)
+{
+    Console.WriteLine(message.ToString());
+    return Task.CompletedTask;
+}
+
+await client.LoginAsync(TokenType.Bot, token);
+
+await client.StartAsync();
+
+await Task.Delay(-1);
