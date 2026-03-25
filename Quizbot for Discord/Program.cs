a93ae896eced303e7a9e5a08fb2017013788122b
h678153 <678153@stud.hvl.no>
2026-03-25 11:18:59 +0100

    Add ping slash command test

--- Quizbot for Discord/Program.cs
+++ Quizbot for Discord/Program.cs
@@ -3,7 +3,7 @@ using Discord;
 using Discord.WebSocket;
 using Microsoft.Extensions.Configuration;
 
-
+// setup
 IConfigurationRoot config = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json")
     .Build();
@@ -17,6 +17,7 @@ var socketConfig = new DiscordSocketConfig
 
 var client = new DiscordSocketClient(socketConfig);
 
+// loghandler
 client.Log += LogHandler;
 
 Task LogHandler(LogMessage message)
@@ -25,8 +26,35 @@ Task LogHandler(LogMessage message)
     return Task.CompletedTask;
 }
 
+// ready handelr
+client.Ready += ReadyHandler;
+
+async Task ReadyHandler()
+{
+    var pingCommand = new SlashCommandBuilder()
+        .WithName("ping")
+        .WithDescription("Pong Ding Dong!");
+    await client.Rest.CreateGuildCommand(pingCommand.Build(), 1485970234971258900); //Custom guild id for testing, CreateGlobalApplicationCommandAsync for global
+}
+
+
+client.SlashCommandExecuted += SlashCommandHandler;
+
+async Task SlashCommandHandler(SocketSlashCommand command)
+{
+    if (command.CommandName == "ping")
+    {
+        await command.RespondAsync("Pong Ding Dong!");
+    }
+}
+
+
+
+//launch
 await client.LoginAsync(TokenType.Bot, token);
 
 await client.StartAsync();
 
 await Task.Delay(-1);
+
+
